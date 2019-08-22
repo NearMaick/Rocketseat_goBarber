cc107fe3963a35de58cabde59d59b517763802a8
Maick Souza <maick_a_s@msn.com>
2019-08-22 13:11:11 -0300

    Aula 15-18 do módulo 2 GoBarber

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '1ad8964708094c',
+    pass: '77f4723a09ceaf',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
