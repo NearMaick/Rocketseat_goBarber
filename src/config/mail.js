591cd8ea9399010e1867a97e860815c6f9036591
Maick Souza <maick_a_s@msn.com>
2019-08-22 16:13:37 -0300

    Final do módulo 2 GoBarber

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '1ad8964708094c',
-    pass: '77f4723a09ceaf',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
