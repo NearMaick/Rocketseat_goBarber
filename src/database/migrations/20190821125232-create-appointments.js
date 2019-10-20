7c07d58e78b596d12730f79e2a0a26d59c1fbe6e
Maick Souza <maick_a_s@msn.com>
2019-10-20 14:10:48 -0300

    fix: migrations

--- src/database/migrations/20190821125232-create-appointments.js
+++ src/database/migrations/20190821125232-create-appointments.js
@@ -16,14 +16,14 @@ module.exports = {
         references: { model: 'users', key: 'id' },
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
-        alloeNull: true,
+        allowNull: true,
       },
       provider_id: {
         type: Sequelize.INTEGER,
         references: { model: 'users', key: 'id' },
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
-        alloeNull: true,
+        allowNull: true,
       },
       canceled_at: {
         type: Sequelize.DATE,
